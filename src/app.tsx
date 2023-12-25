66f902f8dc1e80c389e7e1a9e58caf464ea5a377
max5961 <mmorrissey5961@gmail.com>
2023-12-24 23:02:51 -0500

    Initial commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,84 @@
 import React from "react";
 import { useState } from "react";
 
-export default function App() {
-    return <div id="app">Hello world</div>;
+interface SquareProps {
+    className: string;
+    onClick: () => void;
+}
+function Square({ className, onClick }: SquareProps): React.ReactElement {
+    return <div className={className} onClick={onClick}></div>;
+}
+
+function GameContainer(): React.ReactElement {
+    const board: Array<Array<number>> = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    const [currBoard, setCurrBoard] = useState<Array<Array<number>>>(board.slice());
+
+    function handleSquareClick(key: string): void {
+        const [yIndex, xIndex] = key.split(",");
+        const nextBoard = currBoard.slice();
+
+        let currNumber = nextBoard[yIndex][xIndex];
+        currNumber ? (currNumber = 0) : (currNumber = 1);
+        nextBoard[yIndex][xIndex] = currNumber;
+        setCurrBoard(nextBoard);
+    }
+
+    const squares: Array<React.ReactElement> = currBoard
+        .map((row, yIndex) => {
+            return row.map((square, xIndex) => {
+                const key: string = `${yIndex},${xIndex}`;
+                if (square === 0) {
+                    const className = "square";
+                    return (
+                        <Square
+                            key={key}
+                            className={className}
+                            onClick={() => handleSquareClick(key)}
+                        />
+                    );
+                } else {
+                    const className = "square taken";
+                    return (
+                        <Square
+                            key={key}
+                            className={className}
+                            onClick={() => handleSquareClick(key)}
+                        />
+                    );
+                }
+            });
+        })
+        .flat();
+
+    return (
+        <div id="game-container">
+            <div className="game-board computer"></div>
+            <div className="game-board player">{squares}</div>
+            <div className="sidebar">
+                <button className="random-placement">Random Placement</button>
+                <button className="reset-placement">Reset</button>
+                <div className="ships-container"></div>
+            </div>
+        </div>
+    );
+}
+
+export default function App(): React.ReactElement {
+    return (
+        <>
+            <GameContainer />
+        </>
+    );
 }
