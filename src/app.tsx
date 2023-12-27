864b64b242b827dad34fa919d764bd378b8eb058
max5961 <mmorrissey5961@gmail.com>
2023-12-27 00:03:37 -0500

    Move components and props

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,55 @@
 import React from "react";
 import { useState } from "react";
 
-interface SquareProps {
-    className: string;
-    onClick: () => void;
-}
-function Square({ className, onClick }: SquareProps): React.ReactElement {
-    return <div className={className} onClick={onClick}></div>;
+export default function App(): React.ReactElement {
+    return (
+        <>
+            <Game />
+        </>
+    );
 }
 
-function GameContainer(): React.ReactElement {
-    const board: Array<Array<number>> = new Array(10)
-        .fill(null)
-        .map(() => new Array(10).fill(0));
-
-    const [currBoard, setCurrBoard] = useState<Array<Array<number>>>(
-        board.slice(),
+function Game(): React.ReactElement {
+    const [playerBoard, setPlayerBoard] = useState<Array<Array<number>>>(
+        new Array(10).fill(null).map(() => new Array(10).fill(0)),
     );
 
+    return (
+        <div id="game">
+            <div className="game-board computer"></div>
+            <PlayerBoard
+                playerBoard={playerBoard}
+                setPlayerBoard={setPlayerBoard}
+            />
+            <div className="sidebar">
+                <button className="random-placement">Random Placement</button>
+                <button className="reset-placement">Reset</button>
+                <div className="ships-container"></div>
+            </div>
+        </div>
+    );
+}
+
+interface PlayerBoardProps {
+    playerBoard: Array<Array<number>>;
+    setPlayerBoard: (newBoard: Array<Array<number>>) => void;
+}
+function PlayerBoard({
+    playerBoard,
+    setPlayerBoard,
+}: PlayerBoardProps): React.ReactElement {
     function handleSquareClick(key: string): void {
         const [yIndex, xIndex] = key.split(",");
-        const nextBoard = currBoard.slice();
+        const nextBoard = playerBoard.slice();
 
         let currNumber = nextBoard[yIndex][xIndex];
         currNumber ? (currNumber = 0) : (currNumber = 1);
         nextBoard[yIndex][xIndex] = currNumber;
-        setCurrBoard(nextBoard);
+        setPlayerBoard(nextBoard);
     }
 
     function getComponentSquares(): Array<React.ReactElement> {
-        return currBoard
+        return playerBoard
             .map((row, yIndex) => {
                 return row.map((square, xIndex) => {
                     const key: string = `${yIndex},${xIndex}`;
@@ -57,23 +77,14 @@ function GameContainer(): React.ReactElement {
             .flat();
     }
 
-    return (
-        <div id="game-container">
-            <div className="game-board computer"></div>
-            <div className="game-board player">{getComponentSquares()}</div>
-            <div className="sidebar">
-                <button className="random-placement">Random Placement</button>
-                <button className="reset-placement">Reset</button>
-                <div className="ships-container"></div>
-            </div>
-        </div>
-    );
+    return <div className="game-board player">{getComponentSquares()}</div>;
 }
 
-export default function App(): React.ReactElement {
-    return (
-        <>
-            <GameContainer />
-        </>
-    );
+interface SquareProps {
+    key: string;
+    className: string;
+    onClick: () => void;
+}
+function Square({ key, className, onClick }: SquareProps): React.ReactElement {
+    return <div key={key} className={className} onClick={onClick}></div>;
 }
